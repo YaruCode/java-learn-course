ac7ff9e8f48431104eeef63d7cd843ee02574cf9
YaruCode <3300837618@qq.com>
2022-02-03 12:52:21 +0800

    add some books and some code about how to use map,set,queue,stack

---
+++ src/DS/DS/Tree.java
@@ -0,0 +1,24 @@
+package DS.DS;
+
+public class Tree {
+    private TreeNode _left;
+
+    public TreeNode get_left() {
+        return _left;
+    }
+
+    public TreeNode get_right() {
+        return _right;
+    }
+
+    public void set_left(TreeNode _left) {
+        this._left = _left;
+    }
+
+    public void set_right(TreeNode _right) {
+        this._right = _right;
+    }
+
+    private TreeNode _right;
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.*;
 
+import DS.DS.TreeNode;
 import enter.yasen.*;
 import org.junit.jupiter.api.Test;
 
@@ -9,103 +10,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Animal dog = new Dog();
-//        dog.Eat();
-//        dog.Run();
-//        dog.Shout();
-//
-//        Cat cat = new Cat();
-//        cat.Eat();
-//        cat.Run();
-//        cat.Shout();
-//        cat.set_color(Color.Black);
 
-//        System.out.println(cat.get_color().toString());
+        String str1=new String("hello");
+        String str2=new String("hello");
 
-        //BitSet 数据结构
-//        BitSet set = new BitSet();
-//        set.set(2);
-//
-//        Vector<Integer> vector = new Vector<>();
-//        vector.add(12);
-//        vector.add(22);
-//
-//
-//        while (!vector.isEmpty()) {
-//            System.out.println(vector.elementAt(0));
-//            vector.remove(0);
-//        }
-//        boolean flag = vector.stream().allMatch(x -> x > 3);
-
-//        Collection<String> coll = new ArrayList<>();
-//        coll.add("dog");
-//        coll.add("cat");
-//        coll.add("sheep");
-//
-//        Iterator<String> iter = coll.iterator();
-////       while(iter.hasNext())
-////       {
-////           System.out.println(iter.next());
-////       }
-//
-////        HashSet<String> set=new HashSet<>();
-////        set.add("S");
-////        set.add("c");
-////        set.add("b");
-////        set.add("b");
-////        set.add("b");
-////        set.add("b");
-////        for(String s:set)
-////        {
-////            System.out.println(s);
-////        }
-//
-//        HashMap<String, Integer> map = new HashMap<>();
-//        map.put("one", 1);
-//        map.put("two", 2);
-//        map.put("ten", 10);
-//        map.put("seven", 7);
-//
-//        for (String s : map.keySet()) {
-//        //    System.out.println(map.get(s));
-//        }
-//
-//
-//        LinkedHashMap<String, Integer> map1 = new LinkedHashMap<>();
-//        map1.put("one", 1);
-//        map1.put("six", 6);
-//        map1.put("two", 2);
-//
-//        Integer i=map.get("six");
-//
-//        map1.put("six",i+1);
-//
-//        for(String s:map1.keySet())
-//        {
-//            System.out.println(map1.get(s));
-//        }
-////        for(Map.Entry<String,Integer> entry :map1.entrySet())
-////        {
-////            System.out.println(entry.getValue());
-////        }
-//
-//
-//        Integer[] data=new Integer[10];
-
-//    int a=1;
-//    int b=2;
-//    int res=Add(a,b);
-//    //Incre(res);
-//    Inc(res);
-//    System.out.println(res);
-//TryCatchMethod();
+        System.out.println(str1==str2);
+        System.out.println(str1.equals(str1));
 
 
-        List<Integer> res=new ArrayList<>();
-        res.add(1);
-        assert 2==1:"2！=1";
 
 
+
+
+
+
+
+
+    }
+
+    public static List<Integer> postorderTraversal(TreeNode root) {
+
+        //可以直接用递归的方式来分析
+        //定义一个静态的集合
+        List<Integer> res = new ArrayList<Integer>();
+
+        while(!res.isEmpty())
+        {
+            System.out.println(res.get(0));
+            res.remove(0);
+        }
+
+        return null;
+    }
+
+    public static void ListSet(List<Integer> list) {
+
+        list.add(1);
     }
 
     public static void TryCatchMethod() {
@@ -121,7 +61,7 @@ public class Main {
             System.out.println(ex.getMessage());
         } finally {
 
-System.out.println("finally");
+            System.out.println("finally");
         }
 
 
